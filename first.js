902caf048c66eb4d6ce19056160a3cae1062a448
Rudra Pratap Singh <rudrapratap592005@gmail.com>
2024-10-03 23:59:40 +0530

    Total score count

--- first.js
+++ first.js
@@ -1,3 +1,12 @@
+
+let score ={
+  win : 0,
+  lost : 0,
+  tie : 0,
+}
+
+
+
 function generateComputerChoice() {
   let randomNumber = Math.random() * 3;
 
@@ -13,32 +22,44 @@ function generateComputerChoice() {
 function getResult(userMove, computerMove) {
   if (userMove === "Bat") {
     if (computerMove === "Ball") {
+      score.win++;
       return "user won";
     } else if (computerMove === "Bat") {
+      score.tie++;
       return "tie";
     } else if (computerMove === "Stump") {
+      score.lost++;
       return "computer won";
     }
   } else if (userMove === "Ball") {
     if (computerMove === "Ball") {
+      score.tie++;
       return "tie";
     } else if (computerMove === "Bat") {
+      score.win++;
       return "user won";
     } else if (computerMove === "Stump") {
+      score.lost++;
       return "computer won";
     }
   } else if (userMove === "Stump") {
     if (computerMove === "Bat") {
+      score.win++;
       return "user won";
     } else if (computerMove === "Ball") {
+      score.lost++;
       return "computer won";
     } else if (computerMove === "Stump") {
+      score.tie++;
       return "tie";
     }
   }
 }
 function showResult(userMove, computerMove, result) {
+  
   alert(
-    `You Have Choosen ${userMove} .Computer Choice is ${computerMove} and ${result}`
-  );
+    `You Have Choosen ${userMove} .Computer Choice is ${computerMove} 
+    
+  ${result}
+      win${score.win} lost${score.lost} tie${score.tie} `);
 }
